dd61e0916ef51840a93d95f132757e945b3c933b
Macintosh HD <sokim1616@gmail.com>
2020-07-18 01:34:05 +0900

    files.py & modules.py practice + created myfile.txt

--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,19 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+# Core modules
+import datetime
+from datetime import date
+import time
+from time import time
+
+# Pip module
+from camelcase import CamelCase
+
+# today = datetime.date.today()
+today = date.today()
+timestamp = time()
+
+c = CamelCase()
+print(c.hump("hello there world"))
+
+print(timestamp)
